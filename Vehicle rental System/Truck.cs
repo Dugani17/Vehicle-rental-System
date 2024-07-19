a37d05cc703c5be46fce3b70968fa8bee6dbb467
Prasad Dugani <ldugani4526@conestogac.on.ca>
2024-07-19 18:27:08 -0400

    Truck code

---
+++ Vehicle rental System/Truck.cs
@@ -0,0 +1,17 @@
+﻿// Truck.cs
+public class Truck : Vehicle
+{
+    // Truck-specific properties
+    public double Capacity { get; set; }
+    public string TruckType { get; set; }
+    public bool FourWheelDrive { get; set; }
+
+    // Override DisplayDetails method
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Capacity: {Capacity} tons");
+        Console.WriteLine($"Truck Type: {TruckType}");
+        Console.WriteLine($"Four Wheel Drive: {FourWheelDrive}");
+    }
+}
