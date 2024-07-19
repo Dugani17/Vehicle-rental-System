86541f6e7d5cfecc02119e928c6de0971e7ecfeb
Prasad Dugani <ldugani4526@conestogac.on.ca>
2024-07-19 18:30:39 -0400

    Rental system code

---
+++ Vehicle rental System/RentalAgency.cs
@@ -0,0 +1,44 @@
+﻿// RentalAgency.cs
+using System;
+using System.Collections.Generic;
+
+public class RentalAgency
+{
+    // Properties
+    private List<Vehicle> Fleet { get; set; } = new List<Vehicle>();
+    public decimal TotalRevenue { get; private set; }
+
+    // Methods
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Fleet.Add(vehicle);
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        Fleet.Remove(vehicle);
+    }
+
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        if (Fleet.Contains(vehicle))
+        {
+            Fleet.Remove(vehicle);
+            TotalRevenue += vehicle.RentalPrice * days;
+            Console.WriteLine($"Vehicle rented: {vehicle.Model} for {days} days.");
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
+    }
+
+    public void DisplayFleet()
+    {
+        foreach (var vehicle in Fleet)
+        {
+            vehicle.DisplayDetails();
+            Console.WriteLine();
+        }
+    }
+}
