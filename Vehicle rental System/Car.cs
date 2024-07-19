bb075c2210d91d880bdf1c4621bdd4005498cc1d
Prasad Dugani <ldugani4526@conestogac.on.ca>
2024-07-19 18:24:13 -0400

    Vehicle Rentl Project

---
+++ Vehicle rental System/Car.cs
@@ -0,0 +1,19 @@
+﻿// Car.cs
+public class Car : Vehicle
+{
+    // Car-specific properties
+    public int Seats { get; set; }
+    public string EngineType { get; set; }
+    public string Transmission { get; set; }
+    public bool Convertible { get; set; }
+
+    // Override DisplayDetails method
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Seats: {Seats}");
+        Console.WriteLine($"Engine Type: {EngineType}");
+        Console.WriteLine($"Transmission: {Transmission}");
+        Console.WriteLine($"Convertible: {Convertible}");
+    }
+}
